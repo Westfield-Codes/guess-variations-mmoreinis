e621451c1d9020b0e1e8adb93c0aaf44e467dcdc
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-09-30 14:04:00 +0000

    16

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-/* Define a function called rollDice */
-function rollDice() {
-  // create a variable called roll, set it equal to random 1-6
-  let roll = Math.floor(Math.random()*6+1);
-  // if roll equals 6, display "6! You win!"
-  if (roll == 6) alert("6! You win");  
-  // otherwise:
-  else {
-  // use a confirm and say: "You rolled a [roll]"
-  confirm("You rolled a " + roll);
-  // then call rollDice again.
-  rollDice();
-  // close the else block
-  }
+/* Simple Guess Program */
 
-// close the function block
+// define function guessGame
+function guessGame() {
+  // generate a random integer 1-100
+  var answer = math.random()*100+1;
+  // display the integer for testing purposes only
+  alert(answer);
+  // create guess and set equal to 0 
+var guess = 0;
+  // loop while guess is not equal to answer
+while (guess != answer){
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("guess a number 1-100");
+    // if guess equals answer display "Correct!" 
+if (guess == answer) alert("correct!");
+    // or if guess is greater than answer display "Too high!" 
+
+    // or if guess is less than answer display "Too low!" 
+
+    // else say "Bad input"
+
+  // end the loop
+
+// end the function
 }
